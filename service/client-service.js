7f69e9c6e29f626bc960ad439276dac81995e3cc
lokywolf2295 <matiasesek1995@gmail.com>
2023-01-10 16:46:10 -0300

    I add createclient function to enter new customers with ID

--- service/client-service.js
+++ service/client-service.js
@@ -1,6 +1,17 @@
-const listClients = () =>
+const listClients = () => //crea la conexion con el servidor y obtiene los datos de la lista de clientes
   fetch("http://localhost:3000/profile").then((respuesta) => respuesta.json());
 
-export const clientServices = {
+const createClient = (name, email) => { //funcion que crea el cliente recibiendo los valores de nombre y email
+  return fetch("http://localhost:3000/profile", { //lo direcciona al servidor
+    method: "POST", //y lo agrega en su json
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ name, email, id: uuid.v4() }), //el contenido a agregar es nombre, email y el id obtenido por el script agregado
+  });
+};
+
+export const clientServices = { //exportamos la función padre que contiene las demas funciones
   listClients,
+  createClient,
 };
